9b9838a9bfc7306bc5fdc15e17379c45ef627374
yeongbba <yeongmolee2@gmail.com>
2020-10-24 01:00:53 +0900

    add logout

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React from 'react';
+import { authService } from 'fbase';
+import { useHistory } from 'react-router-dom';
 
-const Profile = (props) => <span>Profile</span>;
+export default () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push('/');
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
